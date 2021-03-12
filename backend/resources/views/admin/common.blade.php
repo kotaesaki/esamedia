a8a41579c3697183c333f66d39de5ff583482362
kotaesaki <gurico1229@gmail.com>
2021-03-12 20:40:46 +0900

    修正

--- backend/resources/views/admin/common.blade.php
+++ backend/resources/views/admin/common.blade.php
@@ -38,7 +38,7 @@
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+  <link href="{{ mix('css/admin.css') }}" rel="stylesheet">
 
   <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
