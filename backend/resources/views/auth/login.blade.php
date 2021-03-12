8b342ce001f07300787b5e21162444303edde79a
kotaesaki <gurico1229@gmail.com>
2021-03-12 20:35:36 +0900

    gitignore

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -87,17 +87,17 @@
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Login') }}
                                     </button>
-
-                                    @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                    @endif
                                 </div>
                             </div>
                         </form>
+
                     </div>
                 </div>
+                <a href="{{route('guest.login')}}">
+                    <div class="btn btn-primary" style="margin-left: 37%; margin-top: 4%;">
+                        ゲストログインボタン
+                    </div>
+                </a>
             </div>
         </div>
     </div>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -31,6 +31,7 @@ Route::group(['prefix' => 'admin'], function () {
     Route::get('/', 'App\Http\Controllers\Admin\HomeController@showHome')->name('admin.showHome');
     Route::get('home', 'App\Http\Controllers\Admin\HomeController@index')->name('admin.home');
     Route::get('login', 'App\Http\Controllers\Admin\LoginController@showLoginForm')->name('admin.login');
+    Route::get('login/guest', 'App\Http\Controllers\Admin\LoginController@guestLogin')->name('guest.login');
     Route::post('login', 'App\Http\Controllers\Admin\LoginController@login')->name('admin.login');
     Route::post('home', 'App\Http\Controllers\Admin\LoginController@logout')->name('admin.logout');
 
@@ -42,6 +43,20 @@ Route::group(['prefix' => 'admin'], function () {
     Route::post('home/new/complete', 'App\Http\Controllers\FormController@newPost')->name('new_post');
     Route::post('home/edit/complete', 'App\Http\Controllers\FormController@editPost')->name('edit_post');
     Route::post('home/delete/{post_id}', 'App\Http\Controllers\FormController@delete')->name('delete_post');
+    Route::get('home/category', 'App\Http\Controllers\CategoryController@index')->name('get_category');
+    Route::post('home/category/create', 'App\Http\Controllers\CategoryController@create')->name('create_category');
+    Route::post('home/category/delete', 'App\Http\Controllers\CategoryController@delete')->name('delete_category');
+    Route::get('home/category/{term_id}/edit', 'App\Http\Controllers\CategoryController@edit')->name('edit_category');
+    Route::post('home/category/update', 'App\Http\Controllers\CategoryController@update')->name('update_category');
+
+    Route::get('home/tag', 'App\Http\Controllers\TagController@index')->name('get_tag');
+    Route::post('home/tag/create', 'App\Http\Controllers\TagController@create')->name('create_tag');
+    Route::post('home/tag/delete', 'App\Http\Controllers\TagController@delete')->name('delete_tag');
+    Route::get('home/tag/{term_id}/edit', 'App\Http\Controllers\TagController@edit')->name('edit_tag');
+    Route::post('home/tag/update', 'App\Http\Controllers\TagController@update')->name('update_tag');
+
+    Route::get('home/comment', 'App\Http\Controllers\CommentController@showAdmin')->name('show_comment');
+    Route::post('home/comment/delete', 'App\Http\Controllers\CommentController@delete')->name('delete_comment');
 });
 
 
@@ -49,22 +64,6 @@ Route::group(['prefix' => 'admin'], function () {
 user->user_status == admin;
 */
 Route::group(['middleware' => ['auth', 'can:system-only']], function () {
-    Route::get('admin/home/category', 'App\Http\Controllers\CategoryController@index')->name('get_category');
-    Route::post('admin/home/category/create', 'App\Http\Controllers\CategoryController@create')->name('create_category');
-    Route::post('admin/home/category/delete', 'App\Http\Controllers\CategoryController@delete')->name('delete_category');
-    Route::get('admin/home/category/{term_id}/edit', 'App\Http\Controllers\CategoryController@edit')->name('edit_category');
-    Route::post('admin/home/category/update', 'App\Http\Controllers\CategoryController@update')->name('update_category');
-
-    Route::get('admin/home/tag', 'App\Http\Controllers\TagController@index')->name('get_tag');
-    Route::post('admin/home/tag/create', 'App\Http\Controllers\TagController@create')->name('create_tag');
-    Route::post('admin/home/tag/delete', 'App\Http\Controllers\TagController@delete')->name('delete_tag');
-    Route::get('admin/home/tag/{term_id}/edit', 'App\Http\Controllers\TagController@edit')->name('edit_tag');
-    Route::post('admin/home/tag/update', 'App\Http\Controllers\TagController@update')->name('update_tag');
-
-    Route::get('admin/home/comment', 'App\Http\Controllers\CommentController@showAdmin')->name('show_comment');
-    Route::post('admin/home/comment/delete', 'App\Http\Controllers\CommentController@delete')->name('delete_comment');
-
-
     Route::get('admin/home/users', 'App\Http\Controllers\UserAdminController@index')->name('show_users');
     Route::post('admin/home/users/delete', 'App\Http\Controllers\UserAdminController@delete')->name('delete_user');
     Route::get('admin/home/users/confirm', 'App\Http\Controllers\UserAdminController@confirm')->name('confirm_user');
